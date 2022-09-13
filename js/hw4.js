93eee8a5b08a1d4ad40651b0a0150b249e3a2c3d
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-09-13 16:15:24 +0300

    Added hw4 until task 29

--- js/hw4.js
+++ js/hw4.js
@@ -86,29 +86,151 @@
 
 
 
-function changeEven(numbers, value) {
-  // Change code below this line
-  const newArray =[];
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const newArray =[];
   
-  numbers.forEach(number => {
-    if (number % 2 === 0) {
-      newArray.push(number + value);
-    } else if (number % 2 !== 0) {
-      newArray.push(number);
-    }
-  })
+//   numbers.forEach(number => {
+//     if (number % 2 === 0) {
+//       newArray.push(number + value);
+//     } else if (number % 2 !== 0) {
+//       newArray.push(number);
+//     }
+//   })
 
-  return console.log(newArray);
+//   return console.log(newArray);
   
-  // for (let i = 0; i < numbers.length; i += 1) {
-  //   if (numbers[i] % 2 === 0) {
-  //     numbers[i] = numbers[i] + value;
-  //   }
-  // }
-  // Change code above this line
-}
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] % 2 === 0) {
+//   //     numbers[i] = numbers[i] + value;
+//   //   }
+//   // }
+//   // Change code above this line
+// }
 
-changeEven([1, 2, 3, 4, 5], 10);
-changeEven([2, 8, 3, 7, 4, 6], 10)
-changeEven([17, 24, 68, 31, 42], 100);
-changeEven([44, 13, 81, 92, 36, 54], 100);
+// changeEven([1, 2, 3, 4, 5], 10);
+// changeEven([2, 8, 3, 7, 4, 6], 10)
+// changeEven([17, 24, 68, 31, 42], 100);
+// changeEven([44, 13, 81, 92, 36, 54], 100);
+
+// // task 21
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter( book => book.rating >=MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+
+// console.log(topRatedBooks);
+// console.log(booksByAuthor);
+
+
+// task 24
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+// const getUsersWithFriend = (users, friendName) => { return users.filter( user =>user.friends.includes(friendName))
+//   // console.log(user.friends);
+
+// };
+
+// console.log(getUsersWithFriend(users, "Briana Decker"));
+
+// task 25
+
+const getFriends = (users) => { return users.flatMap(user =>user.friends).filter((friend,index,array) => array.indexOf(friend) === index)};
+
+
+console.log(getFriends(users));
