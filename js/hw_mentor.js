bdb202a2750734f2ef24d45f072fed93ad1a69e1
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-10-15 16:24:51 +0300

    'addedLesson'

--- js/hw_mentor.js
+++ js/hw_mentor.js
@@ -134,23 +134,23 @@
 
 // console.log(chunkArray(data, 3)); // [[1, 2, 3], [4, 5, 6], [7]]
 
-// // task!
-// // // Напишіть метод, який розбиває масив на частини визначеного розміру
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 6, 17, 18, 19];
-function chunkArray(data, chunk) {
-    const newArr = [];
-    const newData = [...data];
-    newData.forEach((number, index, arr) => {
-        newArr.push(arr.splice(0, chunk));                
-    });
+// // // task!
+// // // // Напишіть метод, який розбиває масив на частини визначеного розміру
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 6, 17, 18, 19];
+// function chunkArray(data, chunk) {
+//     const newArr = [];
+//     const newData = [...data];
+//     newData.forEach((number, index, arr) => {
+//         newArr.push(arr.splice(0, chunk));                
+//     });
 
-    return [...newArr];
-};
+//     return [...newArr];
+// };
 
 
-console.log(chunkArray(data, 2)); // [[1, 2], [3, 4], [5, 6], [7]]
+// console.log(chunkArray(data, 2)); // [[1, 2], [3, 4], [5, 6], [7]]
 
-console.log(chunkArray(data, 3)); // [[1, 2, 3], [4, 5, 6], [7]]
+// console.log(chunkArray(data, 3)); // [[1, 2, 3], [4, 5, 6], [7]]
 
 
 
@@ -319,4 +319,162 @@ console.log(chunkArray(data, 3)); // [[1, 2, 3], [4, 5, 6], [7]]
 // console.log(Math);
 
 
+
+// // За допомогою методу forEach потрібно заповнити масив uploadedFiles
+// // baseUrl потрібно передати, як this
+// // Потім зробити це завдання за допомогою методу map
+
+// const imagesName = ['car.jpeg', 'grass.jpeg', 'avatar.png', 'user.jpeg', 'sea.jpeg', 'water.jpeg', 'bird.jpeg'];
+// const baseUrl = 'domain-name.com';
+// const path = '/upload/';
+// let uploadedFiles = [];
+  
+// imagesName.forEach(function (img) {
+//   uploadedFiles.push(`${this}${path}${img}`)
+  
+// }, baseUrl)
+// console.log(uploadedFiles)
+// uploadedFiles = imagesName.map(img => `${baseUrl}${path}${img}`)
+//  console.log(uploadedFiles)
+
+// uploadedFiles = imagesName.reduce((acc, img) => {
+//   return [...acc, `${baseUrl}${path}${img}`]
+// }, [])
+
+// console.log(uploadedFiles)
+
+
+// const resultUploadedFiles = [
+//   'domain-name.com/upload/car.jpeg',
+//   'domain-name.com/upload/grass.jpeg',
+//   'domain-name.com/upload/avatar.png',
+//   'domain-name.com/upload/user.jpeg',
+//   'domain-name.com/upload/sea.jpeg',
+//   'domain-name.com/upload/water.jpeg',
+//   'domain-name.com/upload/bird.jpeg'
+// ];
+
+
+// / Напишіть функцію addItemToBasket, яка буде за допомогою методу find перевіряти, чи є вже такий продукт в кошику
+// // Якщо він є, то потроібно оновити його в коризні
+// // Якщо немає, то потрібно додати новий продукт до кошику покупок
+
+// Після цього порахувати суму всіх покупок і вивести total;
+
+// let total = 0;
+
+// const tShirt = {
+//   id: 33,
+//   title: 'T-shirt',
+//   price: 99,
+//   count: 1,
+// }
+
+// const jeans = {
+//   id: 29,
+//   title: 'Jeans',
+//   price: 199,
+//   count: 1,
+// }
+
+// let basketItems = [
+//   {
+//     id: 28,
+//     title: 'iPhone 14',
+//     price: 1599,
+//     count: 3,
+//   },
+//   {
+//     id: 29,
+//     title: 'Jeans',
+//     price: 199,
+//     count: 2,
+//   }
+// ];
+
+// function addItemToBasket(newProduct) {
+//     const currentProduct = basketItems.find(product => product.id === newProduct.id);
+//     if (currentProduct) {
+//        return basketItems.map(product => {
+//            if (product.id === currentProduct.id) {
+//                return {
+//                    ...product,
+//                    count: product.count + 1,
+//                }
+//            }
+//            else return product;
+//         })
+//     }
+//     else {
+//         return [...basketItems, newProduct]
+//     }
+
+// }
+
+// basketItems = addItemToBasket(tShirt);
+// basketItems = addItemToBasket(jeans);
+// console.table(basketItems);
+
+// const sum = basketItems
+//     .map(({ price, count }) => price * count)
+//     .reduce((total, product) =>
+//     total += product
+// , 0)
+
+// console.log(sum);
+
+
+// Потрібно отримати URL адресу із адресного рядка бразуера.
+// Далі необхідно параметри запиту записати в об'єкт  (query) - ключ, значення (axios)
+
+// const url = 'https://www.npmjs.com/?query=axios&version=0.3&module=render&size=32&date=12212183311'
+
+// const link = new URL(url)
+// console.log(link)
+
+// const search = link.search
+// console.log(search)
+
+// const arraySearch = search.slice(1).split('&')
+// console.log(arraySearch)
+
+// const paramsObj = arraySearch.reduce((obj, string) => {
+//     console.log(obj)
+//     console.log(string)
+//     const [key, value] = string.split('=')
+//     obj[key] = value;
+//     return obj
+// }, {})
+// console.log(paramsObj)
+
+// Напишіть метод, який очищає масив від усіх непотрібних елементів, таких як false, undefined, порожні рядки, нуль, null
+
+// const data = [0, 1, false, 2, undefined, '', 3, null];
+// console.log(compact(data)) // [1, 2, 3]
+
+
+// Ви маєте масив об'єктів user, і в кожному з них є user.name. Напишіть код, який перетворює їх на масив імен за порядком зростання, за віком.
+// Наприклад:
+
+let jhon = { name: "Jhon", age: 26 };
+let bruce = { name: "Bruce", age: 24 };
+let maria = { name: "Maria", age: 28 };
+
+let users = [ jhon, bruce, maria ];
+
+// let names = /* ... ваш код */
+
+// alert(names); // Bruce, Jhon, Maria
+
+let names = users.sort((prev, next) => {
+   
+    // console.log(prev);
+    // console.log(next);
+
+    return next.age - prev.age;
+})
+    .map(item => item.name);
+
+console.log(users);
+console.log(names);
 
